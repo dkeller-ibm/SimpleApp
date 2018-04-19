4fc749a2433be90a3be016536ac4bbd98b94eae3
DKeller <dmytro.keller1@ibm.com>
2018-04-19 17:03:45 +0300

    init commit

---
+++ SimpleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by DKeller on 4/19/18.
+//  Copyright © 2018 DKeller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
